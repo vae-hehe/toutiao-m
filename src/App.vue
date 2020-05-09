726d52ccda2eda4ab285e1c3ce650400540ec2d2
呵呵 <1840494062@qq.com>
2020-05-09 09:57:39 +0800

    初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
+    <h1>黑马头条</h1>
+    <!-- 路由出口 -->
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 注册路由
 Vue.use(VueRouter)
 
+// 路由规则
 const routes = []
 
+// 初始化路由
 const router = new VueRouter({
   routes
 })
