f1c8a2f1fe9ca31ccc25e72a42bf71ed47c58466
呵呵 <1840494062@qq.com>
2020-05-09 15:58:28 +0800

    登录界面-页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,38 @@
 <template>
-<div class="login-container">登录</div>
+<div class="login-container">
+  <!--
+    left-arrow 左侧箭头
+  -->
+  <van-nav-bar
+    title="登录"
+    left-arrow
+    @click-left="$router.back()"
+  />
+
+  <!-- 登录表单 -->
+  <van-cell-group>
+    <van-field
+      v-model="user.mobile"
+      left-icon="smile-o"
+      placeholder="请输入手机号"
+    />
+    <van-field
+      v-model="user.code"
+      clearable
+      left-icon="music-o"
+      placeholder="请输入验证码"
+    >
+      <template #button>
+        <van-button
+          size="small"
+          round
+        >发送验证码</van-button>
+      </template>
+    </van-field>
+  </van-cell-group>
+  <van-button type="info" block>登录</van-button>
+  <!-- /登录表单 -->
+</div>
 </template>
 
 <script>
@@ -8,7 +41,12 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
