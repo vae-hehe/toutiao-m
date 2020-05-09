059655b4d491294b90744c5da72d069e7499d6fe
呵呵 <1840494062@qq.com>
2020-05-09 10:47:55 +0800

    导入Vant组件库

--- src/main.js
+++ src/main.js
@@ -6,8 +6,19 @@ import store from './store'
 // 加载style全局样式
 import './styles/index.less'
 
+// 加载 Vant 组件库
+import Vant from 'vant'
+
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
+// 创建 vue 根实例, 将 router, store 配置到根实例中
+// 把 App 根组件渲染到 #app 节点
 new Vue({
   router,
   store,
