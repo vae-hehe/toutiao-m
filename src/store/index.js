a9da5af86b0a72503178be2b44f0b71e584bab22
呵呵 <1840494062@qq.com>
2020-05-10 09:58:35 +0800

    我的-单元格导航布局完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
+const USER_KEY = 'toutiao-user'
+
 export default new Vuex.Store({
   state: {
+    user: getItem(USER_KEY) // 当前登录用户的登录状态(token 等数据)
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+
+      // 为了防止页面刷新数据丢失, 把数据放到本地存储中, 仅仅是为了数据持久化
+      setItem(USER_KEY, state.user)
+    }
   },
   actions: {
   },
