c8afa0f44d66a62db483619fcc7bd57e2252f63d
呵呵 <1840494062@qq.com>
2020-05-12 10:32:45 +0800

    我的-样式完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -9,55 +9,65 @@
       :border="false"
     >
       <van-image
+        class="avatar"
         slot="icon"
-        width="50"
-        height="50"
         round
         fit="cover"
         src="https://img.yzcdn.cn/vant/cat.jpeg"
       />
-      <div slot="title">昵称</div>
+      <div slot="title" class="name">昵称</div>
       <van-button
+        class="update-btn"
         size="small"
         round
       >编辑资料</van-button>
     </van-cell>
-    <van-grid :border="false">
-      <van-grid-item>
-        <div slot="text">123</div>
-        <div slot="text">头条</div>
+    <van-grid class="data-info" :border="false">
+      <van-grid-item class="data-info-item">
+        <div class="text-wrap" slot="text">
+          <div class="count">123</div>
+          <div class="text">头条</div>
+        </div>
       </van-grid-item>
-      <van-grid-item>
-        <div slot="text">123</div>
-        <div slot="text">关注</div>
+      <van-grid-item  class="data-info-item">
+        <div class="text-wrap" slot="text">
+          <div class="count">123</div>
+          <div class="text">关注</div>
+        </div>
       </van-grid-item>
-      <van-grid-item>
-        <div slot="text">123</div>
-        <div slot="text">粉丝</div>
+      <van-grid-item class="data-info-item">
+        <div class="text-wrap" slot="text">
+          <div class="count">123</div>
+          <div class="text">粉丝</div>
+        </div>
       </van-grid-item>
-      <van-grid-item>
-        <div slot="text">123</div>
-        <div slot="text">获赞</div>
+      <van-grid-item class="data-info-item">
+        <div class="text-wrap" slot="text">
+          <div class="count">123</div>
+          <div class="text">头条</div>
+        </div>
       </van-grid-item>
     </van-grid>
   </van-cell-group>
 
-  <van-grid :column-num="2">
+  <van-grid class="nav-grid mb-4" :column-num="2">
     <van-grid-item
+      class="nav-grid-item"
       icon-prefix="toutiao"
       icon="shoucang"
-      text="文字"
+      text="收藏"
     />
     <van-grid-item
+      class="nav-grid-item"
       icon-prefix="toutiao"
       icon="lishi"
-      text="文字"
+      text="历史"
     />
   </van-grid>
   <!-- is-link 右边的箭头 -->
-  <van-cell title="消息通知" is-link to="/" />
-  <van-cell title="小智同学" is-link to="/" />
-  <van-cell title="退出登录" />
+  <van-cell class="cell" title="消息通知" is-link to="/" />
+  <van-cell class="mb-4 cell" title="小智同学" is-link to="/" />
+  <van-cell class="logout-cell" title="退出登录" />
 </div>
 </template>
 
@@ -83,11 +93,78 @@ export default {
     background: url() no-repeat;
     background-size: cover;
     .base-info {
+      box-sizing: border-box;
+      height: 115px;
       background-color: unset;
+      padding-top: 38px;
+      padding-bottom: 11px;
+      .avatar {
+        width: 66px;
+        height: 66px;
+        border: 1px solid #fff;
+        margin-right: 11px;
+      }
+      .name {
+        font-size: 15px;
+        color: #fff;
+      }
+      .update-btn {
+        // 建议不设置宽, 文字撑开
+        height: 16px;
+        font-size: 10px;
+        color: #666;
+      }
+    }
+    .data-info {
+      .data-info-item {
+        height: 65px;
+        color: #ffffff;
+        .text-wrap {
+          display: flex;
+          flex-direction: column;
+          justify-items: center;
+          align-items: center;
+          .count {
+            font-size: 18px;
+          }
+          .text{
+            font-size: 11px;
+          }
+        }
+      }
     }
     /deep/ .van-grid-item__content {
       background-color: unset;
     }
+  }
+  .nav-grid {
+    .nav-grid-item {
+      height: 70px;
+      /deep/ .toutiao {
+        font-size: 22px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #333;
+      }
+    }
+  }
+  .cell {
+    font-size: 15px;
+  }
+  .logout-cell {
+    font-size: 15px;
+    text-align: center;
+    color: #d86262;
+  }
+  .mb-4 {
+    margin-bottom: 4px;
   }
 }
 </style>
