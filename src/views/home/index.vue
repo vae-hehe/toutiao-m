8883b7d0f52257566c588b9fc4575a3d2de465f2
呵呵 <1840494062@qq.com>
2020-05-12 14:55:01 +0800

    首页-导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,17 @@
 <template>
-<div class="home-container">首页</div>
+<div class="home-container">
+  <van-nav-bar class="app-nav-bar">
+    <van-button
+      class="search-btn"
+      slot="title"
+      icon="search"
+      type="info"
+      round
+      size="mini"
+    > 搜索
+    </van-button>
+  </van-nav-bar>
+</div>
 </template>
 
 <script>
@@ -18,4 +30,22 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background-color: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
+</style>
