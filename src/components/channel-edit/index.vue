3bca9ed220e7cfa5d5988740311b764d49760fc9
呵呵 <1840494062@qq.com>
2020-05-14 20:23:49 +0800

    频道编辑-删除频道(不是持久化)

--- src/components/channel-edit/index.vue
+++ src/components/channel-edit/index.vue
@@ -19,6 +19,7 @@
       v-for="(channel, index) in userChannels"
       :key="index"
       :text="channel.name"
+      @click="onUserChannelClick(index)"
     />
   </van-grid>
   <!-- /我的频道 -->
@@ -43,6 +44,8 @@
 
 <script>
 import { getAllChannels } from '@/api/article'
+import { mapState } from 'vuex'
+import { setItem } from '@/utils/storage'
 export default {
   name: 'ChannelEdit',
   components: {},
@@ -103,6 +106,33 @@ export default {
       this.userChannels.push(channel)
 
       // 数据持久化未做
+      if (this.user) {
+        // 登录了,数据存储到线上
+      } else {
+        // 没有登录, 数据存储到本地
+        setItem('user-channels', this.userChannels)
+      }
+    },
+    // 删除我的频道的数据
+    onUserChannelClick (index) {
+      if (this.isEdit && index !== 0) {
+        // 如果是编辑状态, 而且下标不等于0, 就删除数据
+        // 在外面封装删除函数
+        this.deleteChannel(index)
+      } else {
+        // 不是编辑状态, 就切换频道
+        this.switchChannel(index)
+      }
+    },
+    // 删除频道
+    deleteChannel (index) {
+      this.userChannels.splice(index, 1)
+
+      // 数据持久化
+    },
+    // 切换频道
+    switchChannel (index) {
+      console.log(11)
     }
   }
 }
